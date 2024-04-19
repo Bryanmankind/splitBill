c12a2dd2c131a711b1af24efaea0bc3a8da960a6
Bryanmankind <shegeemankind@gmail.com>
2024-04-19 15:13:47 +0100

    splitBill

--- splitbill/src/App.js
+++ splitbill/src/App.js
@@ -44,6 +44,9 @@ function ListFriends ({friend}) {
       <img src={friend.image} alt={friend.name} />
       <h3>{friend.name}</h3>
       {friend.balance < 0 && (<p className="red">You owe {friend.name} 💲{Math.abs(friend.balance)}</p>)}
+      {friend.balance > 0 && (<p className="green"> {friend.name} owes you 💲{Math.abs(friend.balance)}</p>)}
+      {friend.balance === 0 && (<p>You and {friend.name} are even</p>)}
+      <button className="button">Select</button>
     </li>
   )
 }
